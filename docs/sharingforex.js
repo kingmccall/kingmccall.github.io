cbbd8a605c37e12a31e878d7bdbf75f5bb56057f
PeacefulWiser <bryanwagoki2016@gmail.com>
2022-11-25 15:50:08 +0300

    Remove space and add chatroom

--- docs/sharingforex.js
+++ docs/sharingforex.js
@@ -54,3 +54,28 @@ userName.value = getLocalStorageData.username;
 Email.value = getLocalStorageData.email;
 Msg.value = getLocalStorageData.message;
 
+// --------------------------------------
+/*the complete project is in the following link:
+https://github.com/vkive/colofur-rain.git
+Follow me on Codepen
+*/
+function rain(){
+  let amount = 100;
+  let body = document.querySelector('body');
+  let i = 0;
+   while(i < amount){
+     let drop = document.createElement('i');
+     let size = Math.random()*5;
+     let posX = Math.floor(Math.random()*window.innerWidth);
+     let delay = Math.random()*-20;
+     let duration = Math.random()*5
+
+     drop.style.width =0.2 + size +'px';
+     drop.style. left = posX + 'px';
+     drop.style.animationDelay = delay+'s';
+     drop.style.animationDuration = 1+duration+'s';
+     body.appendChild(drop);
+     i++
+}
+}
+rain();
